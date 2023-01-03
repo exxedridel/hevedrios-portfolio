98f0d9f7876744c63df2090fc8f188016fbd70fe
hevedrios <exxed.ridel@gmail.com>
2023-01-02 21:35:18 -0600

    Hotfix removed unused variables to avoid production warnings

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import AppContext from "../Context/AppContext";
 import { GridContainerPortfolio } from "../Styles/Portfolio.styles";
