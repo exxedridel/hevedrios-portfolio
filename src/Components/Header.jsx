d80e4345eb346949fc0ed6666880e4c270a09b62
exxedridel <exxed.ridel@gmail.com>
2023-04-30 23:24:17 -0600

    ADD mobile close functionality when option is clicked or outside of the menu is pressed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react"
+import { useState, useContext, useEffect, useRef } from "react"
 import AppContext from "../Context/AppContext"
 import { Link } from "react-router-dom"
 import { SkipToContent, Logo, HeaderStyled } from "../Styles/Header.styles"
@@ -9,6 +9,18 @@ import iconClose from "../Assets/Shared/icon-close.svg"
 const Header = () => {
    const { mainFocus, homeActive, portfolioActive, servicesActive, aboutActive } = useContext(AppContext);
    const [toggle, setToggle] = useState(false)
+   const refMenu = useRef(null)
+
+   useEffect(() => {
+      document.addEventListener("click", handleClickOutside, true)
+   }, [])
+
+   function handleClickOutside(e) {
+      if (!refMenu.current.contains(e.target)) {
+         // toggleClick()
+         setToggle(false)
+      }
+   }
 
    function toggleClick() { setToggle(!toggle) }
 
@@ -19,19 +31,19 @@ const Header = () => {
    return (
       <>
          <SkipToContent onClick={mainFocus}>Skip to content</SkipToContent>
-         <HeaderStyled className="primary-header flex">
+         <HeaderStyled className="primary-header flex" ref={refMenu}>
             <div>
                <Logo src={logo} alt="Heved logo" />
             </div>
             <button className="mobile-nav-toggle" onClick={toggleClick} style={iconMenu} aria-controls="primary-navigation">
                <span className="sr-only" aria-expanded={toggle}>Menu</span>
             </button>
-            <nav >
+            <nav>
                <ul id="primary-navigation" data-visible={toggle} className="primary-navigation underline-indicators flex">
-                  <li className={homeActive}><Link to="/" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">00</span>Home</Link></li>
-                  <li className={portfolioActive}><Link to="portfolio" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">01</span>Portfolio</Link></li>
-                  <li className={servicesActive}><Link to="services" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">02</span>Services</Link></li>
-                  <li className={aboutActive}><Link to="about" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">03</span>About</Link></li>
+                  <li className={homeActive} onClick={toggleClick}><Link to="/" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">00</span>Home</Link></li>
+                  <li className={portfolioActive} onClick={toggleClick}><Link to="portfolio" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">01</span>Portfolio</Link></li>
+                  <li className={servicesActive} onClick={toggleClick}><Link to="services" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">02</span>Services</Link></li>
+                  <li className={aboutActive} onClick={toggleClick}><Link to="about" className="uppercase ff-sans-cond text-white letter-spacing-2"><span aria-hidden="true">03</span>About</Link></li>
                </ul>
             </nav>
          </HeaderStyled>
