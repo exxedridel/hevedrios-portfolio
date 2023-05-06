137baced79b5f90a975d51d7281b8a4464613cf9
exxedridel <exxed.ridel@gmail.com>
2023-05-06 02:21:04 -0600

    ADD full descriptions for the about page

--- src/Data/aboutData.js
+++ src/Data/aboutData.js
@@ -5,16 +5,16 @@ const data1 = [
         subtitle: "BCS.",
         linkName: "Resume",
         description:
-            "A front-end developer with 3 years of experience. I am passionate about learning and implementing modern coding and front-end techniques over my projects. Here is my resume for the details.",
+            "A front-end developer with 3 years of experience. I am passionate about learning and implementing modern coding and front-end techniques to get the best of my web projects. If you want to know more about me here is my resume.",
         isCurrent: true,
     },
     {
         id: "1",
-        title: "COMPUTER SCIENCE",
-        subtitle: "BACHELOR in",
+        title: "COMPUTER SYSTEMS",
+        subtitle: "Engineer in",
         linkName: "Certificate",
         description:
-            "I hold a degree in Computer Systems Engineering, which is equivalent to a Bachelor's degree in Computer Science. In addition, I also have a degree in Public Administration, which has allowed me to develop strong soft skills.",
+            "I hold a degree in Computer Systems Engineering by UTEL, where aquired programing and software development skills. In addition, I also have a degree in Public Administration, which has allowed me to develop strong soft skills.",
         isCurrent: false,
     },
     {
@@ -23,7 +23,7 @@ const data1 = [
         subtitle: "Scrimba",
         linkName: "Certificate",
         description:
-            "My passion for front-end engineering stems from my love for both mathematics and creating visually appealing designs. I realized early on that I wanted to combine these two interests to build beautiful and functional web applications.",
+            "I discovered that front-end development combines two of my greatest interests to work in: logical-mathematical thinking and visually appealing designs, being so I decided to go ahead to the next level with the Scrimba's certification.",
         isCurrent: false,
     },
     {
@@ -32,7 +32,7 @@ const data1 = [
         subtitle: "EF SET",
         linkName: "Certificate",
         description:
-            "Correct! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione iste unde, eveniet ex cum pariatur earum magni hic accusantium eos exercitationem beatae nostrum voluptatibus praesentium aliquid atque sit id totam.",
+            "My native language is spanish since I was born in Mexico, however, to learn proper english has always been an interest to me to take advantage of all the benefits that this entails, but also to effectively communicate with more people.",
         isCurrent: false,
     },
 ];
